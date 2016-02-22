61686ac4d0ca0d26231b3dd82bb62596161c1227
Wernimont, Martin R <mwernimont@usgs.gov>
2016-02-22 10:25:22 -0600

    better function and cdnjs for handlebars

--- js/application.js
+++ js/application.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 	
-	var headerTemplate = $.get('js/templates/header.handlebars', function(data){
-		var compiledTemplate = Handlebars.compile(data);
-		var html = compiledTemplate({});
-		$('#header').html(html);
-	});
+	var template = function(){
+		$.each(files, function(index, value){
+			//Gets mustache file
+			$.get(value, function(data){
+				var compiledTemplate = Handlebars.compile(data);
+				var html = compiledTemplate({});
+				//Places mustache file in correct location
+				$(divs[index]).html(html);
+			});
+		});
+	};
 	
-	var footerTemplate = $.get('js/templates/footer.handlebars', function(data){
-		var compiledTemplate = Handlebars.compile(data);
-		var html = compiledTemplate({});
-		$('#footer').html(html);
-	});
+	var files = ['js/templates/header.handlebars', 'js/templates/footer.handlebars', 'js/templates/nav.handlebars'];
+
+	var divs = ['#header', '#footer', '#nav'];
 	
-	var navTemplate = $.get('js/templates/nav.handlebars', function(data){
-		var compiledTemplate = Handlebars.compile(data);
-		var html = compiledTemplate({});
-		$('#nav').html(html);
-	});
-	
+	template();
+
 });
+
