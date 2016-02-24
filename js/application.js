2077fd6de3692a20925c014302fe1883c1153e03
Wernimont, Martin R <mwernimont@usgs.gov>
2016-02-24 10:13:51 -0600

    star generator

--- js/application.js
+++ js/application.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 	
-	var sections = [
-		{file:'js/templates/header.handlebars', div: '#header'}, 
-		{file:'js/templates/footer.handlebars', div: '#footer'},
-		{file:'js/templates/nav.handlebars', div: '#nav'} 
-	];
+	var postworkshop = $.getJSON('json/postworkshop.json', function(data){
+		var reviews = $.map(data, function(row){
+				row.stars = [];
+				for(var i = 0; i < row.satisfaction; i++){
+					row.stars[i] = i;
+				}
+				return row;
+			});
+		var sections = [
+			{file:'js/templates/header.handlebars', div: '#header', context: {}}, 
+			{file:'js/templates/footer.handlebars', div: '#footer', context: {}},
+			{file:'js/templates/nav.handlebars', div: '#nav', context: {}} ,
+			{file:'js/templates/review.handlebars', div: '#productReview', context: reviews}
+		];
 	
 		$.each(sections, function(index, value){
 			//Gets mustache file
 			$.get(value.file, function(data){
 				var compiledTemplate = Handlebars.compile(data);
-				var html = compiledTemplate({});
+				var html = compiledTemplate(value.context);
 				//Places mustache file in correct location
 				$(value.div).html(html);
 			});
 		});
+	});
 });
 
