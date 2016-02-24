0f3a7754f4f79add989a15175d92b1b7d13f2155
Lindsay Carr <lcarr@usgs.gov>
2016-02-24 14:08:07 -0600

    update reston attendees

--- js/map.js
+++ js/map.js
@@ -15,7 +15,7 @@ var mtwsc = L.marker([46.584066, -112.042416]).addTo(map);
 //Popups for locations above
 marker.bindPopup("<h4>Florida Water Science Center</h4><p>12 Attendees</p>");
 nmwsc.bindPopup("<h4>New Mexico Water Science Center</h4><p>15 Attendees</p>");
-egsc.bindPopup("<h4>Eastern Geographic Science Center</h4><p>600,000 Attendees!!</p>");
+egsc.bindPopup("<h4>Eastern Geographic Science Center</h4><p>18 Attendees</p>");
 mtwsc.bindPopup("<h4>Wyoming-Montana Water Science Center</h4><p>16 Attendees</p>");
 
 //Map tile
