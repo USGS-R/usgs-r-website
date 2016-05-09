47cedfbd6a0fdc1bd1eca6ef1e02bf635412560b
Lindsay Carr <lcarr@usgs.gov>
2016-05-09 16:49:18 -0500

    add FORT to the map

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 //Marks location
 var map = L.map('map').setView([39.409258, -98.569441], 3);
 
-var marker = L.marker([26.077432, -80.247741]).addTo(map);
+var flwsc = L.marker([26.077432, -80.247741]).addTo(map);
 
 var nmwsc = L.marker([35.129985, -106.582949]).addTo(map);
 
@@ -12,11 +12,14 @@ var egsc = L.marker([38.910566, -77.361228]).addTo(map);
 
 var mtwsc = L.marker([46.584066, -112.042416]).addTo(map);
 
+var fort = L.marker([40.559101, -105.082592]).addTo(map);
+
 //Popups for locations above
-marker.bindPopup("<h4>Florida Water Science Center</h4><p>11 Attendees</p>");
+flwsc.bindPopup("<h4>Florida Water Science Center</h4><p>11 Attendees</p>");
 nmwsc.bindPopup("<h4>New Mexico Water Science Center</h4><p>16 Attendees</p>");
 egsc.bindPopup("<h4>Eastern Geographic Science Center</h4><p>20 Attendees</p>");
 mtwsc.bindPopup("<h4>Wyoming-Montana Water Science Center</h4><p>16 Attendees</p>");
+fort.bindPopup("<h4>FORT Science Center</h4><p>29 Attendees</p>");
 
 //Map tile
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
